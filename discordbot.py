74d8596c5c673c1d12827f47820995bfa29f3a9d
김동인/검색데이터개발팀/11ST <kim.dongin@sk.com>
2023-03-03 10:12:19 +0900

    discord bot by python * modfiy 시간 확인

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}. ({datetime.now()})')
+    print(f'Logged in as {client.user}. ({datetime.now()}, UTC : {datetime.utcnow()})')
 
 
 @client.event
